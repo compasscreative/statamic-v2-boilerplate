824fc154b56f97a948e9ec5a5a2d75a6f12e168e
Jeremy Hoover <jeremyhoover@Jeremys-MacBook-Pro.local>
2020-06-16 13:43:13 -0400

    Added responsive bg sizes

--- public/site/themes/main/src/js/vendor/lazysizes.js
+++ public/site/themes/main/src/js/vendor/lazysizes.js
@@ -1,6 +1,26 @@
-document.addEventListener('lazybeforeunveil', function(e) {
-  var bg = e.target.getAttribute('data-bg');
-  if (bg) {
-    e.target.style.backgroundImage = 'url(' + bg + ')';
+function loadLazyBg(target, bestImage, betterImage, okImage, fallbackImage) {
+  var bg = bestImage
+    ? bestImage
+    : betterImage
+    ? betterImage
+    : okImage
+    ? okImage
+    : fallbackImage;
+
+  target.style.backgroundImage = "url(" + bg + ")";
+}
+
+document.addEventListener("lazybeforeunveil", function (e) {
+  var bgSm = e.target.getAttribute("data-bgSm");
+  var bgLg = e.target.getAttribute("data-bgLg");
+  var bgXl = e.target.getAttribute("data-bgXl");
+  var bg = e.target.getAttribute("data-bg");
+
+  if (window.innerWidth > 1400) {
+    loadLazyBg(e.target, bgXl, bgLg, bgSm, bg);
+  } else if (window.innerWidth > 768) {
+    loadLazyBg(e.target, bgLg, bgSm, bgXl, bg);
+  } else {
+    loadLazyBg(e.target, bgSm, bgLg, bgXl, bg);
   }
 });
